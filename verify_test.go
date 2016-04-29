06de6fa4b126e49bd226bca2bcc7ce510e0a983d
Alex Nichol <unixpickle@gmail.com>
2016-04-29 09:28:19 -0400

    added 3x3 test case

--- verify_test.go
+++ verify_test.go
@@ -40,6 +40,41 @@ func Test2x2System(t *testing.T) {
 	}
 }
 
+func Test3x3Solution(t *testing.T) {
+	system := &System{
+		Size: 3,
+		Coefficients: []DiffPoly{
+			{-1, 0, 1}, {2, 2}, {1, 1},
+			{1, -2, 1}, {0, 4}, {-1, 2},
+			{-1, 1}, {2}, {1, -1},
+		},
+		Outputs: []NumFunc{zeroNumFunc, zeroNumFunc, zeroNumFunc},
+	}
+	solution := &Solution{
+		Funcs:        []DiffFunc{first3x3Solution, first2x2Solution, second2x2Solution},
+		NumConstants: 4,
+	}
+	verification := Verify(system, solution, 1000, nil)
+	if !verification[0] {
+		t.Error("did not successfully verify equation 1")
+	}
+	if !verification[1] {
+		t.Error("did not successfully verify equation 2")
+	}
+	if !verification[2] {
+		t.Error("did not successfully verify equation 2")
+	}
+
+	badSolution := &Solution{
+		Funcs:        []DiffFunc{second2x2Solution, first2x2Solution, first3x3Solution},
+		NumConstants: 4,
+	}
+	verification = Verify(system, badSolution, 1000, nil)
+	if verification[0] && verification[1] && verification[2] {
+		t.Error("false positive")
+	}
+}
+
 func second2x2Solution(x *autodiff.DeepNum, consts []*autodiff.DeepNum) *autodiff.DeepNum {
 	// k1 + k2*exp(-t)
 	return consts[0].Add(consts[1].Mul(x.MulScaler(-1).Exp()))
@@ -52,6 +87,20 @@ func first2x2Solution(x *autodiff.DeepNum, consts []*autodiff.DeepNum) *autodiff
 	term1 := consts[2].Mul(expInv)
 	term2 := consts[1].MulScaler(-0.5).Mul(x).Mul(expInv)
 	return term1.Add(term2)
+}
+
+func first3x3Solution(x *autodiff.DeepNum, consts []*autodiff.DeepNum) *autodiff.DeepNum {
+	// k4*e^t - k2/2*t*e^-t + (3/4k2 + k3)e^-t + k1
+	coeff1 := consts[3]
+	coeff2 := consts[1].MulScaler(-1.0 / 2.0)
+	coeff3 := consts[1].MulScaler(3.0 / 4.0).Add(consts[2])
+	coeff4 := consts[0]
+
+	term1 := x.Exp().Mul(coeff1)
+	term2 := coeff2.Mul(x).Mul(x.MulScaler(-1).Exp())
+	term3 := coeff3.Mul(x.MulScaler(-1).Exp())
+	term4 := coeff4
+	return term1.Add(term2).Add(term3).Add(term4)
 }
 
 func zeroNumFunc(float64) float64 {
